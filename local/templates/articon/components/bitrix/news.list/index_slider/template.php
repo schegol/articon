28797d0ca29c2466e7591907fae0163f7819633b
VladProfitkit <vlad@profitkit.ru>
2023-08-12 12:14:53 +0400

    Добавление видео в слайдер на главной

--- local/templates/articon/components/bitrix/news.list/index_slider/template.php
+++ local/templates/articon/components/bitrix/news.list/index_slider/template.php
@@ -16,7 +16,7 @@ $this->setFrameMode(true);
                                 <div class="swiper-wrapper">
                                     <?foreach ($arResult['ITEMS'] as $arItem):?>
                                         <div class="mp-one-slide swiper-slide">
-                                            <div class="mp-one-slide-item">
+                                            <div class="mp-one-slide-item<?=!strlen($arItem['PROPERTIES']['MAIN_IMG']['VALUE']) && strlen($arItem['PROPERTIES']['VIDEO']['VALUE']) ? ' mp-one-slide-item--video' : ''?>">
                                                 <?if (strlen($arItem['PROPERTIES']['BG_IMG']['VALUE'])):?>
                                                     <div class="mp-one-slide-item__bg">
                                                         <img class="image" src="<?=CFile::GetPath($arItem['PROPERTIES']['BG_IMG']['VALUE'])?>" alt="<?=GetMessage('NEWS_LIST_INDEX_SLIDER_BG_ALT')?>">
@@ -24,7 +24,7 @@ $this->setFrameMode(true);
                                                 <?endif?>
 
                                                 <?if (strlen($arItem['PROPERTIES']['TEXT']['VALUE'])):?>
-                                                    <div class="h1 mp-one-slide-item__title">
+                                                    <div class="h1 mp-one-slide-item__title<?=$arItem['PROPERTIES']['WHITE_HEADING']['VALUE'] == 'Y' ? ' mp-one-slide-item__title--white' : ''?>">
                                                         <?=$arItem['PROPERTIES']['TEXT']['VALUE']?>
                                                     </div>
                                                 <?endif?>
@@ -36,6 +36,12 @@ $this->setFrameMode(true);
                                                 ?>
                                                     <div class="mp-one-slide-item__image">
                                                         <img class="image" src="<?=$smallPic?>" alt="<?=$arItem['PROPERTIES']['TEXT']['VALUE']?>">
+                                                    </div>
+                                                <?elseif (strlen($arItem['PROPERTIES']['VIDEO']['VALUE'])):?>
+                                                    <div class="mp-one-slide-item__video">
+                                                        <video class="mainscreen__video" autoplay loop muted<?// data-was-processed="true"?>>
+                                                            <source src="<?=CFile::GetPath($arItem['PROPERTIES']['VIDEO']['VALUE'])?>"<?// data-src="video/fuelet_nft_1600x1600_05.mp4"?>>
+                                                        </video>
                                                     </div>
                                                 <?endif?>
                                             </div>
